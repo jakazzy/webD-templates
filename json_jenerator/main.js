62324ac416eb13dd322c1afd44729654532d8332
jida <jakazzy@gmail.com>
2019-05-01 18:19:26 +0000

    add the query selectors

---
+++ json_jenerator/main.js
@@ -0,0 +1,4 @@
+const jsonButton = document.querySelector('#generate');
+const buttonContainer = document.querySelector('#buttonContainer');
+const display = document.querySelector('#displayContainer');
+const collection = ['another', 'more', 'next', 'continue', 'keep going', 'click me', 'a new one'];
