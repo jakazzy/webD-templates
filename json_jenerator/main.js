f937eaf1095409dbda925728097b4595e03eb538
jida <jakazzy@gmail.com>
2019-05-01 18:29:47 +0000

    add request function

--- json_jenerator/main.js
+++ json_jenerator/main.js
@@ -2,3 +2,18 @@ const jsonButton = document.querySelector('#generate');
 const buttonContainer = document.querySelector('#buttonContainer');
 const display = document.querySelector('#displayContainer');
 const collection = ['another', 'more', 'next', 'continue', 'keep going', 'click me', 'a new one'];
+
+const generateJson = () => {
+    const xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
+
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            renderResponse(xhr.response);
+            changeButton;
+        }
+    }
+
+    xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
+    xhr.send();
+}
