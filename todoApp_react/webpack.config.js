09e736f913e62ff55e4bf917041ecad9614507de
jida <jakazzy@gmail.com>
2019-05-12 14:12:03 +0000

    correct webpack config

--- todoApp_react/webpack.config.js
+++ todoApp_react/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: path.resolve(__dirname, 'src') + '/app/index.js',
     output: {
         path: path.resolve(__dirname, 'dist') + '/app',
-        fileName: bundle.js,
+        filename: bundle.js,
         publicPath: '/app/'
     },
     module: {
