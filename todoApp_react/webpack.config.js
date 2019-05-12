9e4015c423d41989b0cd2caedd37f3eacb24dea1
jida <jakazzy@gmail.com>
2019-05-12 13:59:22 +0000

    add webpack config

---
+++ todoApp_react/webpack.config.js
@@ -0,0 +1,10 @@
+var path = require('path');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src') + '/app/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist') + '/app',
+        fileName: bundle.js,
+        publicPath: '/app/'
+    },
+}
